4d5fe58963606effa7f6f1a3ffaac6ff81286405
bod <1360799362@qq.com>
2019-03-27 00:15:11 +0800

    Dart语法 同java语法进行比较

--- lib/operator_arithmetic.dart
+++ lib/operator_arithmetic.dart
@@ -1,8 +1,13 @@
 void main() {
-  int a = 10;
+  int a = 11;
   int b = 2;
   int c = 10;
 
+  //取得商 得到的是5
+  var i = a ~/ b;
+
+  //取得全部的数据 得到的是5.5
+  var d = a / b;
   //=-*/% 运算符号
   print(b % 10);
   print(++b);
@@ -11,5 +16,24 @@ void main() {
   //判断对象是否相等有其他的方法 == ，这里和java 有所不同
 
   print(a == c);
+
+  //&& 和java 相同 略...
+
+  //逻辑运算符
+  var str = "String";
+  print(str.isEmpty);
+
+  //赋值运算符号 =,??=
+
+  c ??= 30; //c如果有值的话 就用自己的 不然的话就使用30作为其返回值
+
+  b *= c;
+  //三目运算符号 同java
+
+  String aq;
+  String bq ="java";
+  String cq = aq??bq;
+  print("??"+"左边为空的情况下  就使用右边的值作为返回: "+cq);
+  //if 运算符号 同java 略
 
 }
